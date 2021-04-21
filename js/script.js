79a667953fc511d26bccfd2e675ec1c2a1aa1e02
Patrick Coxall <mr.coxall@mths.ca>
2021-04-21 14:25:09 +0000

    linting fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
+/* exported myButtonClicked */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This program displays "Hello, World!"
-
-/* exported myButtonClicked */
 
 function myButtonClicked () {
   document.getElementById('hello-world').innerHTML = 'Hello, World!'
