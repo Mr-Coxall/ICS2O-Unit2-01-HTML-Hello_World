d360699e3887e3208789c616ded4fd6761c63979
Patrick Coxall <mr.coxall@mths.ca>
2021-04-25 20:28:14 +0000

    lint check

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 // Created on: Sep 2020
 // This program displays "Hello, World!"
 
-const clickFunction = myButtonClicked
+// const clickFunction = myButtonClicked
 
 function myButtonClicked () {
   document.getElementById('hello-world').innerHTML = 'Hello, World!'
