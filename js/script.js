f42b40ca79507e14edba4d67ca59c9f181b7893e
Patrick Coxall <mr.coxall@mths.ca>
2021-05-03 01:54:32 +0000

    comment update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This program displays "Hello, World!"
+// This file contains the JS functions for index.html
 
 function myButtonClicked () {
+  // this function displays "Hello, World!"
   document.getElementById('hello-world').innerHTML = 'Hello, World!'
 }
